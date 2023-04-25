c13c76442a3d2e79a04ae82b842331653518b1f8
chryssakr <chryssakaratza@gmail.com>
2023-04-25 14:30:31 +0000

    transfer code in myApp.js which fixes issues

--- database.js
+++ database.js
@@ -3,26 +3,6 @@ let mongoose = require('mongoose');
 const server = '127.0.0.1'; //192.168.68.108
 const database = 'MongoMongoose';
 
-const Schema = mongoose.Schema;
-const personSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  age: Number,
-  favoriteFoods: [String]
-});
-
-
-module.exports = mongoose.model('Person', personSchema);
-let PersonModel = require('./name');
-let Person = new PersonModel({
-    name: "Chryssa",
-    age: 26,
-    favoriteFoods: ["fries", "salads"];
-});
-
-
 
 class Database {
   constructor() {
